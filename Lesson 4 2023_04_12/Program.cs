cee390f4458cd7d720a94fa2e12d7a601bba141f
С - 3 < - 3@DESKTOP-E0FTS21>
2023-04-12 22:18:04 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4 2023_04_12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_2023_04_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string number;
+            int tmp;
+            //bool IsCorrect = true;
+                Console.WriteLine("введите 6 значное число:");
+                number = Console.ReadLine();
+                try
+                {
+                    for (int i = 0; i < number.Length; i++)
+                    {
+                        tmp = int.Parse(number[i].ToString());
+                        if (tmp % 2 == 0)
+                        {
+                        Console.WriteLine(tmp);
+                        }
+                    }
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("ввод не корректен");
+                }               
+         Console.ReadKey();*/
+
+            /*DateTime startTime = DateTime.Now;
+            Console.WriteLine(startTime.ToString("yyyy.MM.dd HH:mm:ss.fff"));
+            Console.ReadKey();
+            DateTime stopTime = DateTime.Now;
+            TimeSpan timeSpan = stopTime - startTime;
+            Console.WriteLine($"\nПрошло {timeSpan.ToString("ss")} секунд и {timeSpan.ToString("fff")}");
+            Console.ReadKey();*/
+            
+            
+        }
+    }
+}
